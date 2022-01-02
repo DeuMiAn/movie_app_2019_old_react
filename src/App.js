5b1f0c4bca425d57e62983fecd46d6ba2b620520
DeuMiAn <66052290+DeuMiAn@users.noreply.github.com>
2022-01-03 02:23:24 +0900

    movie part

--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
 import React from "react";
+import axios from "axios";
 import PropTypes from "prop-types";
 
 class App extends React.Component{
-  constructor(props){
-    super(props);
-    console.log("응애 나 태어남")
-  }
   state={
-    count:0
+    isLoading:true,
+    movies:[],
   }
-  add=()=>{
-    this.setState(current=>({count: current.count+1}))
-  };
-  minus=()=>{
-    this.setState(cur=>({count:cur.count-1}))
-
-  };
-  componentDidMount(){ //component가 처음 render될 때 호출됨
-    console.log("첫 그림 다 그렸달까...")
+  getMovies= async()=>{
+    const movies =await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json")
+    
   }
-  componentDidUpdate(){ //coponet가 업데이트될 때 호출됨
-    console.log("방금 막 수정함")
-  }
-  componentWillUnmount(){
-    console.log("여기까지인가 보오..")
+  componentDidMount(){ //처음렌더링 될떄 호출
+    this.getMovies();
   }
   render(){
-    console.log("그.린.다")
+    const{isLoading}=this.state;//ES6 JS 문법
     return <div>
-      <h1>The number is {this.state.count}</h1>
-      <button onClick={this.add}>add</button>
-      <button onClick={this.minus}>minus</button>
-      
+      {isLoading? "Loading...":"we are ready"}
       </div>
   }
 }
